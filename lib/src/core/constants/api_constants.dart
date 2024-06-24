19783bc6b8e81c6f40b61c8e78059c5812c05810
Rishu Kumar <rishukumar.prince@gmail.com>
2024-06-24 22:36:34 +0530

    refactor

--- lib/src/core/constants/api_constants.dart
+++ lib/src/core/constants/api_constants.dart
@@ -3,12 +3,14 @@ class APIConstants {
 
   // static const baseUrl = 'https://legal-referral.onrender.com/api';
 
-  static const baseUrl = 'http://192.168.29.66:8080/api';
+  // static const baseUrl = 'http://192.168.29.66:8080/api';
 
-  // static const baseUrl =
-  // 'http://ec2-13-233-195-79.ap-south-1.compute.amazonaws.com:8080/api';
+  static const baseUrl =
+      'http://ec2-13-126-240-161.ap-south-1.compute.amazonaws.com:8080/api';
 
   // static const host = 'legal-referral.onrender.com';
   // static const host = '192.168.29.66:8080';
-  static const host = 'legal-referral.onrender.com';
+  // static const host = 'legal-referral.onrender.com';
+  static const host =
+      'ec2-13-126-240-161.ap-south-1.compute.amazonaws.com:8080';
 }
